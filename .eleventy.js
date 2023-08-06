cff2c6c6060e984d02692ebd021009a9cd326400
Alicja Slepowronska <slepowro@hm.edu>
2023-08-06 21:41:38 +0200

    rebased cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,21 @@
-
-
-
 const Image = require("@11ty/eleventy-img");
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime");
 
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(`./src${src}`, {
     widths: [300, 800, null],
     formats: ["avif", "jpeg"],
     urlPath: "/images/",
-    outputDir: "./public/images/"
+    outputDir: "./public/images/",
   });
 
   let imageAttributes = {
     alt,
     sizes,
     loading: "lazy",
-    decoding: "async"
+    decoding: "async",
   };
 
   return Image.generateHTML(metadata, imageAttributes);
@@ -29,23 +27,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" }); //favicons in root
 
-
   eleventyConfig.addShortcode("date", () => `${new Date().getUTCDate}`);
   eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
 
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPlugin(pluginRss);
-
+  eleventyConfig.addPlugin(emojiReadTime, { showEmoji: false });
 
   return {
     dir: {
       input: "src",
-      output: "public"
-    }
+      output: "public",
+      markdownTemplateEngine: "njk"
+    },
   };
-  
-
-  
-
-
 };
--- src/images/blinkies/filelister.py
+++
@@ -1,6 +0,0 @@
-import os
-print("const files=[")
-for file in os.listdir():
-	if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".gif"):
-		print("\"/images/blinkies/" + file + "\",")
-print("]")
