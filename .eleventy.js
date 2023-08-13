779b60086f1b4cd05f0529f0709adb88f53f1f6d
emlos <slepowronska02@gmail.com>
2023-08-13 22:02:00 +0200

    easier html shortcode added

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const Image = require("@11ty/eleventy-img");
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime");
+const markdownIt = require("./src/markdown.js");
+const outdent = require("outdent");
 
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(`./src${src}`, {
@@ -21,26 +23,36 @@ async function imageShortcode(src, alt, sizes) {
   return Image.generateHTML(metadata, imageAttributes);
 }
 
+function markdownToHtmlShortcode(children, tag, class_ = false, id = false) {
+  const content = markdownIt.render(children);
+  return outdent`<${tag} ${class_ ? `class="${class_}"` : ""} ${
+    id ? `id="${id}"` : ""
+  }>${content}</${tag}>`;
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addPassthroughCopy("./src/images/");
-  eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" }); //favicons in root
+  eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" }); //favicons remap to root
   eleventyConfig.addPassthroughCopy("./src/scripts/*.js");
 
+  //shortcodes
   eleventyConfig.addShortcode("date", () => `${new Date().getUTCDate}`);
-
+  eleventyConfig.addPairedShortcode("tag", markdownToHtmlShortcode);
   eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
 
+  //filters
   eleventyConfig.addNunjucksFilter("isLongerThan", (content, threshold) => {
     return content.length > threshold;
-   
   });
-  
+
+  //plugins
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(emojiReadTime, { showEmoji: false });
 
+  eleventyConfig.setLibrary("md", markdownIt);
   return {
     dir: {
       input: "src",
