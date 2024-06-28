cb4415287167461f21a8bd516b5c26f3ac4ca09c
emlos <slepowronska02@gmail.com>
2024-06-28 17:45:20 +0200

    feature: paracelsus dress down minigame! [possibly not working] NSFW

--- src/_data/minigames.js
+++ src/_data/minigames.js
@@ -73,6 +73,66 @@ module.exports = {
         `Feels good. Keep goin'.`,
         `Never cared much fer looks.`,
         `Oh, I think one's still a bit bloody, careful.`,
+    paracelsus: {
+      base: util.readImagePathsFromFolder(
+        base,
+        "../images/minigames/stripgame/paracelsus/"
+      )[0],
+      standard: util.readImagePathsFromFolder(
+        base,
+        "../images/minigames/stripgame/paracelsus/standard"
+      ),
+
+      outfits: {
+        backstory: util.readImagePathsFromFolder(
+          base,
+          "../images/minigames/stripgame/paracelsus/backstory"
+        ),
+        angel: util.readImagePathsFromFolder(
+          base,
+          "../images/minigames/stripgame/paracelsus/angel"
+        ),
+      },
+
+      speech: [
+      `Ah! Such strange sensation...`,
+      `This method is quite unorthodox.`,
+      `Have you no shame? Wear gloves, for light's sake!`,
+      `The doctor is *in*, yes.`,
+      `*surprised gasp*`,
+      `Cold hands! Cold hands!`,
+      `All this is sterile - against the miasma.`,
+      `This is a fascinating experiment.`,
+      `I- suppose if it's for the good of science...`,
+      `Right there! There's all the nerves...`,
+      `Feeling positively sanguine, heheh...`,
+      `You know you have quite a...striking anatomy~`,
+      `Oh! Didn't even know that part existed!`,
+      `*inquisitive humming*`,
+      `D-did the thief slip me something?`,
+      `Your hands...such exquisite craftsmanship`,
+      `Of course my knives are clean...why do you ask?`,
+      `Do I seem attractive to you? I used a new tonic.`,
+      `L-lots of things to learn outside of a book.`,
+      `...pulse quickens… humours churn… body reacts…`,
+      `Ah, my favorite experiment!`,
+      `Did you wash your hands?`,
+      `...oxytocin and vasopressin... o-oh!`,
+      `I hope there's more where that came from...`,
+      `This? Keeps away the miasma.`,
+      `These juvenile pastimes do lighten the mood so!`,
+      `I miss my leeches...`,
+      `Mind the vials!`,
+      `My *ekhm* instruments require precise edges...`,
+      `Hehe, they used to say I had *naked* ambition.`,
+      `The Eastern peoples have this one ritual...`,
+      `Time to put my learnings to practice.`,
+      `Don't be shy, I have tinctures against bites!`,
+      `There's some tools in my kit back there.`,
+      `Mh... don't stop! Doctors orders.`,
+      `I get you 'need' it, but slow down...you'll tear a stitch.`,
+      `'Subject reports flushing-' hm? Carry on!`,
+      `This was all the rage at the university.`
       ],
     },
   },
